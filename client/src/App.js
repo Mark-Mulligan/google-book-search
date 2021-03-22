4b7993d226918c2657a904e34aee4674d58e304e
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-03-22 18:08:12 -0500

    got react router working

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SearchPage from "./pages/SearchPage";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={LoginPage} />
+        <Route exact path="/search" component={SearchPage} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
